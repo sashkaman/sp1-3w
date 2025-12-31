e2b1dd0e5744709d5be8e8ac986cce7b870c484f
Alexander <rzn.russia4912@gmail.com>
2025-12-31 20:14:22 +0300

    inputRef.current.value = '' 	modified:   src/TodolistItem.tsx

--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -17,14 +17,18 @@ export const TodolistItem = ({ title, tasks, deleteTask, changeFilter, createTas
   return (
     <div>
       <h3>{title}</h3>
+
       <div>
         <input ref={inputRef} />
+
         <Button title={'+'} onClick={() => {
           if (inputRef.current) {
             createTask(inputRef.current.value)
+            inputRef.current.value = ''
           }
         }} />
       </div>
+
       {tasks.length === 0 ? (
         <p>Тасок нет</p>
       ) : (
@@ -40,11 +44,13 @@ export const TodolistItem = ({ title, tasks, deleteTask, changeFilter, createTas
           })}
         </ul>
       )}
+
       <div>
         <Button title={'All'} onClick={() => changeFilter('all')} />
         <Button title={'Active'} onClick={() => changeFilter('active')} />
         <Button title={'Completed'} onClick={() => changeFilter('completed')} />
       </div>
+
     </div>
   )
 }
